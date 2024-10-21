5639163dec955ace71804791ff9b56a79f4d6903
davidplay7 <davidk1k3k@gmail.com>
2024-10-21 12:04:30 -0300

    add v1 notify

---
+++ src/notify/v1/notify.js
@@ -0,0 +1,8 @@
+function closeNotification() {
+    document.getElementById("notification").style.display = "none";
+}
+
+
+setTimeout(() => {
+    document.getElementById("notification").style.display = "none";
+}, 5000);
