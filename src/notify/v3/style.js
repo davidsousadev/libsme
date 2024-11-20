204c0732240736ae7b608ca38b61798ac6edf3d7
davidsousadev <davidk1k3k@gmail.com>
2024-11-20 11:06:32 -0300

    Update v3 notify: erro de url

--- src/notify/v3/style.js
+++ src/notify/v3/style.js
@@ -1,6 +1,6 @@
 const link = document.createElement("link");
 link.rel = 'stylesheet';
-link.href = '/src/notify/v3/notify.css'; 
+link.href = 'https://libsme.vercel.app/src/notify/v3/notify.css'; 
 document.head.appendChild(link);
 
 export default link;
